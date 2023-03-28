705a08a298a91db4427108f56cba4e639a68832e
Man Woo Jeon <mjeon@microsoft.com>
2023-03-28 22:46:15 +0900

    testing todollar

--- Program.cs
+++ Program.cs
@@ -1,6 +1,5 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
-var counter = 0;
 
 if (app.Environment.IsDevelopment())
 {
@@ -8,10 +7,10 @@ if (app.Environment.IsDevelopment())
 }
 
 app.MapGet("/", () => "This is a sample web app demonstrates Minimal API.");
-app.MapGet("/showwelcometext", (string userName) =>
+/*app.MapGet("/showwelcometext", (string userName) =>
 {
     return $"Hello {userName}, Welcome to App Service X DevOps session!";
-});
+});*/
 app.MapGet("/todollar", (int krw) =>
 {
     return $"{krw} KRW is {krw*1300} USD.";
