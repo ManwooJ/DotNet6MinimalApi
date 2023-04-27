f695460d31421b5a2d828668e9eb7a67685d2fb3
Man Woo Jeon <mjeon@microsoft.com>
2023-04-27 14:27:05 +0900

    Change APIs

--- Program.cs
+++ Program.cs
@@ -10,14 +10,14 @@ if (app.Environment.IsDevelopment())
 app.MapGet("/", () => "This is a sample web app demonstrates Minimal API.");
 
 // Show welcome message for user name
-/*app.MapGet("/showwelcometext", (string userName) =>
+app.MapGet("/showwelcometext", (string userName) =>
 {
     return $"Hello {userName}, Welcome to App Service X DevOps session!";
-});*/
+});
 
 // Convert currency from USD to KRW
-app.MapGet("/tokoreawon", (int usd) =>
+/*app.MapGet("/tokoreawon", (int usd) =>
 {
     return $"{usd} USD is {usd * 1300} KRW.";
-});
+});*/
 app.Run();
