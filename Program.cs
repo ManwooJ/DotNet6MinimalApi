3556cfffc31925d2605b7448cdba732d3f007f14
Man Woo Jeon <mjeon@microsoft.com>
2023-04-27 09:35:00 +0900

    Added comments

--- Program.cs
+++ Program.cs
@@ -6,11 +6,16 @@ if (app.Environment.IsDevelopment())
     app.UseDeveloperExceptionPage();
 }
 
+// Default message
 app.MapGet("/", () => "This is a sample web app demonstrates Minimal API.");
+
+// Show welcome message for user name
 /*app.MapGet("/showwelcometext", (string userName) =>
 {
     return $"Hello {userName}, Welcome to App Service X DevOps session!";
 });*/
+
+// Convert currency from USD to KRW
 app.MapGet("/tokoreawon", (int usd) =>
 {
     return $"{usd} USD is {usd * 1300} KRW.";
